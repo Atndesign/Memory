362e65b30381c381fbed39862d47d024ad72688a
Kevin <Atnnetworkk@gmail.com>
2020-04-13 16:36:20 +0200

    add states, and handlecardflip

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,17 +5,29 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      gameSize: {
+        z: 3,
+        y: 3,
+      },
       cardWidth: 200,
       nbrFlipped: 0,
       nbrPairs: 0,
       currentFlipped: [],
       flipped: [],
+      isFlipped: 0,
     };
   }
   generateCards = (cardsNbr) => {};
 
   checkCards = (currentFlipped) => {};
-  handleCardFlip = () => {};
+  handleCardFlip = (event) => {
+    let card = event.currentTarget.children[0];
+    if (card.classList.contains("flipped")) {
+      card.classList.remove("flipped");
+    } else {
+      card.classList.add("flipped");
+    }
+  };
 
   render() {
     return (
@@ -24,9 +36,11 @@ class Main extends Component {
           <h1 className="title">Memory</h1>
           <div className="cards-container">
             <Card
+              data={"hello"}
               width={this.state.cardWidth}
               height={this.state.cardWidth}
               handleCardFlip={this.handleCardFlip}
+              isFlipped={this.state.isFlipped}
             />
           </div>
         </header>
