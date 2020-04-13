ede65f458a86025760261e4a12b05d5e135843b2
Kevin <Atnnetworkk@gmail.com>
2020-04-13 22:02:41 +0200

    Updates

---
+++ precache-manifest.85625790c06a5b006cf269d0d0db484b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86ddfd6b068caf81b7486232fda7158",
+    "url": "/Memory/index.html"
+  },
+  {
+    "revision": "0457c1298216bc51b148",
+    "url": "/Memory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de2be4d6cc50889a7f71",
+    "url": "/Memory/static/js/2.f96afff4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Memory/static/js/2.f96afff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0457c1298216bc51b148",
+    "url": "/Memory/static/js/main.19cddebe.chunk.js"
+  },
+  {
+    "revision": "59fcaece451169dfda4a",
+    "url": "/Memory/static/js/runtime-main.5f54cda8.js"
+  }
+]);
